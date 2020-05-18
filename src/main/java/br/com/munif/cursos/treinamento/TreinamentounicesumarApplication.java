03534c6b1cf2b47888898fe9fc98bf148568417e
Munif <munifgebara@gmail.com>
2020-05-18 16:59:24 -0300

    first commit

---
+++ src/main/java/br/com/munif/cursos/treinamento/TreinamentounicesumarApplication.java
@@ -0,0 +1,13 @@
+package br.com.munif.cursos.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentounicesumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentounicesumarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/munif/cursos/treinamento/TreinamentounicesumarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.munif.cursos.treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentounicesumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
