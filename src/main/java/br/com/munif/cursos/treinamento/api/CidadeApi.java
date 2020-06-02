d738fa983b2c47bca1bb4d3e610a4ee5e76de02f
Munif <munifgebara@gmail.com>
2020-06-01 21:52:08 -0300

    PUT POST DELETE

--- src/main/java/br/com/munif/cursos/treinamento/api/CidadeApi.java
+++ src/main/java/br/com/munif/cursos/treinamento/api/CidadeApi.java
@@ -2,15 +2,21 @@ package br.com.munif.cursos.treinamento.api;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.munif.cursos.treinamento.entidades.Cidade;
@@ -44,6 +50,38 @@ public class CidadeApi {
 		}
 		return ResponseEntity.notFound().build();
     }
+	
+	@DeleteMapping(value = "/{id}")
+    public ResponseEntity<Cidade> apagaUm(@PathVariable  String id) {
+		Optional<Cidade> cid=respositorioCidade.findById(id);
+		if (!cid.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		respositorioCidade.delete(cid.get());
+		return  ResponseEntity.ok(cid.get());
+    }
+	
+    @PostMapping
+    public ResponseEntity<Cidade> novo(@RequestBody Cidade nova) {
+    	Cidade cidadeSalva= respositorioCidade.save(nova);
+        return new ResponseEntity <>(cidadeSalva, HttpStatus.CREATED);
+    }
+	
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Cidade> alterar(@PathVariable("id") String id, @RequestBody Cidade novoValorCidade) {
+		Optional<Cidade> valorVelhoDeCidade=respositorioCidade.findById(id);
+		if (!valorVelhoDeCidade.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		novoValorCidade.setId(id);
+		novoValorCidade.setCd(valorVelhoDeCidade.get().getCd());
+		novoValorCidade.setUd(new Date());
+    	Cidade cidadeSalva= respositorioCidade.save(novoValorCidade);
+        return new ResponseEntity <>(cidadeSalva, HttpStatus.OK);
+    }
+	
+	
+	
 	
 	
 }
--- src/main/java/br/com/munif/cursos/treinamento/entidades/BaseEntity.java
+++ src/main/java/br/com/munif/cursos/treinamento/entidades/BaseEntity.java
@@ -48,4 +48,81 @@ public class BaseEntity {
         version = null;
     }
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getImportInfo() {
+		return importInfo;
+	}
+
+	public void setImportInfo(String importInfo) {
+		this.importInfo = importInfo;
+	}
+
+	public Date getCd() {
+		return cd;
+	}
+
+	public void setCd(Date cd) {
+		this.cd = cd;
+	}
+
+	public Date getUd() {
+		return ud;
+	}
+
+	public void setUd(Date ud) {
+		this.ud = ud;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseEntity other = (BaseEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+    
+    
+
 }
